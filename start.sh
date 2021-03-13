3af754d3e78911e238fbddccdf89e68b8dc4985c
abangfash2 <80558751+abangfash2@users.noreply.github.com>
2021-03-13 03:32:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-chmod +x chmod +x cpuminer-sse2
+chmod +x cpuminer-sse2
 
-./xmrig -o pool.supportxmr.com:443 -u 897CSsM74aV8knm5WUAafAQ5HZ4adUBPx4tkxvyC45myXwwgRdKs8mje3qPPqrZvbC1SQ3pBvZYvXgHP35vJDmEpHUConUf -k --tls -p kacrut
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u 897CSsM74aV8knm5WUAafAQ5HZ4adUBPx4tkxvyC45myXwwgRdKs8mje3qPPqrZvbC1SQ3pBvZYvXgHP35vJDmEpHUConUf.vhacx
 
 
 done
